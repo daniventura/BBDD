154910da432db89a0ad2f6b238629b50e60ead00
daniventura <fradaniel.ventura@gmail.com>
2022-05-05 20:31:49 +0200

    cases consistencia update

--- archivos/esquema.sql
+++ archivos/esquema.sql
@@ -128,20 +128,20 @@ CREATE TABLE `envasado` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `liquidobasico`
+-- Estructura de tabla para la tabla `liquidoBasico`
 --
 
-CREATE TABLE `liquidobasico` (
+CREATE TABLE `liquidoBasico` (
   `nombreLiquido` varchar(20) NOT NULL,
   `codBase` varchar(4) NOT NULL,
   `codAroma` varchar(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Volcado de datos para la tabla `liquidobasico`
+-- Volcado de datos para la tabla `liquidoBasico`
 --
 
-INSERT INTO `liquidobasico` (`nombreLiquido`, `codBase`, `codAroma`) VALUES
+INSERT INTO `liquidoBasico` (`nombreLiquido`, `codBase`, `codAroma`) VALUES
 ('Apple Punch', '0014', '0001'),
 ('Cookies & Crème', '0004', '0016'),
 ('Don Juan Reserva', '0008', '0014'),
@@ -150,20 +150,20 @@ INSERT INTO `liquidobasico` (`nombreLiquido`, `codBase`, `codAroma`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `liquidofinal`
+-- Estructura de tabla para la tabla `liquidoFinal`
 --
 
-CREATE TABLE `liquidofinal` (
+CREATE TABLE `liquidoFinal` (
   `codLiquido` varchar(4) NOT NULL,
   `nombreLiquido` varchar(20) NOT NULL,
   `concentracionNicotina` enum('0,3mg/mL','0,6mg/mL','1.2mg/mL') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Volcado de datos para la tabla `liquidofinal`
+-- Volcado de datos para la tabla `liquidoFinal`
 --
 
-INSERT INTO `liquidofinal` (`codLiquido`, `nombreLiquido`, `concentracionNicotina`) VALUES
+INSERT INTO `liquidoFinal` (`codLiquido`, `nombreLiquido`, `concentracionNicotina`) VALUES
 ('0001', 'Apple Punch', '0,3mg/mL'),
 ('0002', 'Apple Punch', '0,6mg/mL'),
 ('0003', 'Apple Punch', '1.2mg/mL'),
@@ -210,19 +210,19 @@ INSERT INTO `maceracion` (`nombreLiquido`, `concentracionNicotina`, `tmacerado`)
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `mezclaaromas`
+-- Estructura de tabla para la tabla `mezclaAromas`
 --
 
-CREATE TABLE `mezclaaromas` (
+CREATE TABLE `mezclaAromas` (
   `aroma` varchar(4) NOT NULL,
   `aromaCompuesto` varchar(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Volcado de datos para la tabla `mezclaaromas`
+-- Volcado de datos para la tabla `mezclaAromas`
 --
 
-INSERT INTO `mezclaaromas` (`aroma`, `aromaCompuesto`) VALUES
+INSERT INTO `mezclaAromas` (`aroma`, `aromaCompuesto`) VALUES
 ('0001', '0015'),
 ('0002', '0015'),
 ('0003', '0014'),
@@ -334,17 +334,17 @@ ALTER TABLE `envasado`
   ADD KEY `FkrecipienteEnvasado` (`codRecipiente`);
 
 --
--- Indices de la tabla `liquidobasico`
+-- Indices de la tabla `liquidoBasico`
 --
-ALTER TABLE `liquidobasico`
+ALTER TABLE `liquidoBasico`
   ADD PRIMARY KEY (`nombreLiquido`),
   ADD KEY `FkbaseLiquidoBasico` (`codBase`),
   ADD KEY `FkaromaLiquidoBasico` (`codAroma`);
 
 --
--- Indices de la tabla `liquidofinal`
+-- Indices de la tabla `liquidoFinal`
 --
-ALTER TABLE `liquidofinal`
+ALTER TABLE `liquidoFinal`
   ADD PRIMARY KEY (`codLiquido`),
   ADD UNIQUE KEY `UkliquidoFinal` (`nombreLiquido`,`concentracionNicotina`),
   ADD KEY `FknicotinaLiquidoFin` (`concentracionNicotina`);
@@ -357,9 +357,9 @@ ALTER TABLE `maceracion`
   ADD KEY `IndexconcentracionNicotina` (`concentracionNicotina`);
 
 --
--- Indices de la tabla `mezclaaromas`
+-- Indices de la tabla `mezclaAromas`
 --
-ALTER TABLE `mezclaaromas`
+ALTER TABLE `mezclaAromas`
   ADD PRIMARY KEY (`aroma`,`aromaCompuesto`),
   ADD KEY `Fkmezclaaroma2` (`aromaCompuesto`);
 
@@ -385,40 +385,40 @@ ALTER TABLE `recipiente`
 -- Filtros para la tabla `colores`
 --
 ALTER TABLE `colores`
-  ADD CONSTRAINT `FkliquidoColores` FOREIGN KEY (`codLiquido`) REFERENCES `liquidofinal` (`codLiquido`),
+  ADD CONSTRAINT `FkliquidoColores` FOREIGN KEY (`codLiquido`) REFERENCES `liquidoFinal` (`codLiquido`),
   ADD CONSTRAINT `FkrecipienteColores` FOREIGN KEY (`codRecipiente`) REFERENCES `recipiente` (`codRecipiente`);
 
 --
 -- Filtros para la tabla `envasado`
 --
 ALTER TABLE `envasado`
-  ADD CONSTRAINT `FkliquidoEnvasado` FOREIGN KEY (`codLiquido`) REFERENCES `liquidofinal` (`codLiquido`),
+  ADD CONSTRAINT `FkliquidoEnvasado` FOREIGN KEY (`codLiquido`) REFERENCES `liquidoFinal` (`codLiquido`),
   ADD CONSTRAINT `FkrecipienteEnvasado` FOREIGN KEY (`codRecipiente`) REFERENCES `recipiente` (`codRecipiente`);
 
 --
--- Filtros para la tabla `liquidobasico`
+-- Filtros para la tabla `liquidoBasico`
 --
-ALTER TABLE `liquidobasico`
+ALTER TABLE `liquidoBasico`
   ADD CONSTRAINT `FkaromaLiquidoBasico` FOREIGN KEY (`codAroma`) REFERENCES `aroma` (`codAroma`),
   ADD CONSTRAINT `FkbaseLiquidoBasico` FOREIGN KEY (`codBase`) REFERENCES `base` (`codBase`);
 
 --
--- Filtros para la tabla `liquidofinal`
+-- Filtros para la tabla `liquidoFinal`
 --
-ALTER TABLE `liquidofinal`
+ALTER TABLE `liquidoFinal`
   ADD CONSTRAINT `FknicotinaLiquidoFin` FOREIGN KEY (`concentracionNicotina`) REFERENCES `maceracion` (`concentracionNicotina`),
-  ADD CONSTRAINT `FknliquidoLiquidoFinal` FOREIGN KEY (`nombreLiquido`) REFERENCES `liquidobasico` (`nombreLiquido`);
+  ADD CONSTRAINT `FknliquidoLiquidoFinal` FOREIGN KEY (`nombreLiquido`) REFERENCES `liquidoBasico` (`nombreLiquido`);
 
 --
 -- Filtros para la tabla `maceracion`
 --
 ALTER TABLE `maceracion`
-  ADD CONSTRAINT `FknliquidoMaceracion` FOREIGN KEY (`nombreLiquido`) REFERENCES `liquidobasico` (`nombreLiquido`);
+  ADD CONSTRAINT `FknliquidoMaceracion` FOREIGN KEY (`nombreLiquido`) REFERENCES `liquidoBasico` (`nombreLiquido`);
 
 --
--- Filtros para la tabla `mezclaaromas`
+-- Filtros para la tabla `mezclaAromas`
 --
-ALTER TABLE `mezclaaromas`
+ALTER TABLE `mezclaAromas`
   ADD CONSTRAINT `Fkmezclaaroma1` FOREIGN KEY (`aroma`) REFERENCES `aroma` (`codAroma`),
   ADD CONSTRAINT `Fkmezclaaroma2` FOREIGN KEY (`aromaCompuesto`) REFERENCES `aroma` (`codAroma`);
 
