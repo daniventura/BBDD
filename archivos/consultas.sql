2fd2cfd8469c3edaf8bce5d18de433a14704ad5f
daniventura <fradaniel.ventura@gmail.com>
2022-05-10 18:30:14 +0200

    xquery

--- archivos/consultas.sql
+++ archivos/consultas.sql
@@ -9,47 +9,47 @@ USE fabricaeliquidExportada;
 SELECT codLiquido 'Código de Líquido' FROM colores
 WHERE color LIKE '00,30,81,11';
 
-  /*###########################################
-    Consulta sobre dos tablas creada sin join
-    --------------------------------------
-    Códigos de los líquidos que tienen un tiempo de macerado mayor a 3 semanas, ordenados de mayor tiempo a menor, mostrando este también
-  ###########################################*/
+/*###########################################
+  Consulta sobre dos tablas creada sin join
+  --------------------------------------
+  Códigos de los líquidos que tienen un tiempo de macerado mayor a 3 semanas, ordenados de mayor tiempo a menor, mostrando este también
+###########################################*/
 SELECT liquidoFinal.codLiquido 'Código de líquido', CONCAT(maceracion.tmacerado, ' ', 'días') 'Tiempo de macerado' FROM liquidoFinal, maceracion
 WHERE liquidoFinal.nombreLiquido = maceracion.nombreLiquido
 AND maceracion.tmacerado > '21'
 ORDER BY maceracion.tmacerado;
 
-  /*###########################################
-    Consulta sobre dos tablas creada con join
-    --------------------------------------
-    Proporción en la base con la que se elabora el líquido Don Juan Reserva
+/*###########################################
+  Consulta sobre dos tablas creada con join
+  --------------------------------------
+  Proporción en la base con la que se elabora el líquido Don Juan Reserva
   ###########################################*/
 SELECT base.proporcion 'Proporción de Base' FROM base
 JOIN liquidoBasico ON base.codBase = liquidoBasico.codBase
 WHERE liquidoBasico.nombreLiquido = 'Don Juan Reserva';
 
-  /*###########################################
-    Consulta de más de dos tablas creada con join
-    --------------------------------------
-    Nombres de líquidos que utilicen aromas de la categoria frutal
+/*###########################################
+  Consulta de más de dos tablas creada con join
+  --------------------------------------
+  Nombres de líquidos y códigos que utilicen aromas de la categoria frutal
   ###########################################*/
-SELECT DISTINCT liquidoFinal.nombreLiquido 'Nombre de líquido' FROM liquidoFinal
+SELECT DISTINCT liquidoFinal.nombreLiquido 'Nombre de líquido', liquidoFinal.codLiquido 'Código' FROM liquidoFinal
 JOIN liquidoBasico ON liquidoFinal.nombreLiquido = liquidoBasico.nombreLiquido
 JOIN aroma on liquidoBasico.codAroma = aroma.codAroma
 WHERE aroma.categoria = 'frutal';
 
-  /*###########################################
-    Consulta con cálculos sobre agrupación de filas
-    --------------------------------------
-    Media de dias, redondeados a números enteros, de maceración según la concentracion de nicotina en los líquidos
+/*###########################################
+  Consulta con cálculos sobre agrupación de filas
+  --------------------------------------
+  Media de dias, redondeados a números enteros, de maceración según la concentracion de nicotina en los líquidos
   ###########################################*/
 SELECT concentracionNicotina 'Concentración de nicotina', CONCAT(ROUND(AVG(tmacerado), 0), ' ', 'días') 'Días de maceración' FROM maceracion
 GROUP BY concentracionNicotina;
 
-  /*###########################################
-    Consulta con subconsultas
-    --------------------------------------
-    Enumera los códigos de base las cuales poseen tanto la molécula de 'frescor' como la de 'golpe de garganta'
+/*###########################################
+  Consulta con subconsultas
+  --------------------------------------
+  Enumera los códigos de base las cuales poseen tanto la molécula de 'frescor' como la de 'golpe de garganta'
   ###########################################*/
 SELECT DISTINCT codBase 'Código de Base' FROM moleculas
 WHERE codBase IN
---
+++ archivos/consultas.xq
@@ -0,0 +1,53 @@
+(: Muestra la tabla liquidobasico sin etiquetas :)
+for $liquido in //liquidobasico
+return $liquido/node()/text(),
+
+(: lista las categorias de aromas sin valores repetidos :)
+for $categoria in distinct-values(//aroma/categoria)
+return $categoria,
+
+(: Según cada concentración de nicotina, muestra la media de dias de maceracion de los liquidos :)
+for $maceracion in //maceracion
+for $tmacerado in $maceracion/tmacerado
+group by $nicotina := $maceracion/concentracionNicotina
+return "para " || $nicotina || ", media de " || avg($tmacerado) || " días",
+
+(: Nombres y códigos de aromas ordenados alfabeticamente:)
+for $aroma in //aroma[categoria='frutal']
+order by $aroma/aroma
+return $aroma/aroma || " " || $aroma/codAroma,
+
+(: Nombres y códigos de líquidos que utilicen aromas de la categoria frutal :)
+let $aroma := //aroma[categoria='frutal']/codAroma
+for $liquido in //liquidobasico[codAroma=$aroma]/nombreLiquido
+for $liquidofinal in //liquidofinal[nombreLiquido=$liquido]
+return $liquidofinal/nombreLiquido || " " || $liquidofinal/codLiquido,
+
+(: Lista en forma de tabla html los aromas y las categoria a la que pertenecen, además de indicar el total de ellos :)
+<html>
+<table border="1">
+  <tr>
+    <th>Código aroma</th>
+    <th>Aroma</th>
+    <th>Categoría</th>
+  </tr>
+  {
+  for $aroma in /database/aroma 
+  
+  return <tr>
+    <td>{$aroma/aroma/text()} </td>
+    <td>{$aroma/categoria/text()} </td>
+  </tr>
+  }
+  {
+  let $total := count (/database/aroma)
+  return <tr>
+    <td colspan="2">TOTAL</td>
+    <td >{$total}</td>
+  </tr>
+  }
+</table>
+</html>
+  
+  
+  
